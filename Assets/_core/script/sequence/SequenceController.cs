728da3842891912637feea6dc81c7ef47f3d5fd3
Kelen Nguyen <manhhieu012@gmail.com>
2023-10-01 13:10:05 +0700

    sequence on attack

--- Assets/_core/script/sequence/SequenceController.cs
+++ Assets/_core/script/sequence/SequenceController.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class SequenceController : MonoBehaviour
 {
+    [SerializeField] UnityEvent onSequencesStartToPlay;
     [SerializeField] List<SequenceBase> sequences;
 
     public void PlaySequences()
     {
+        onSequencesStartToPlay?.Invoke();
         foreach (var sequence in sequences)
         {
             StartCoroutine(sequence.GetSequence());
